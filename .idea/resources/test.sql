e6478ac17880ac23f5e13b97183c706e1cd7032e
Dogisday01 <abzal.nurym028@gmail.vom>
2023-02-08 12:03:57 +0600

    test attempt 3

---
+++ .idea/resources/test.sql
@@ -0,0 +1,3 @@
+insert into users (name, age, email) values ('Abzal', 18, 'abzal.nurym028@gmail.com	');
+#delete from users;
+#update users set name = 'Asqar', age = 19 where id = 1;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.sql.*;
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Ok");
-    }
-}
+        Scanner in = new Scanner(System.in);        System.out.print("Enter user name: ");
+        String name = in.next();        System.out.print("Enter password: ");
+         int pass = in.nextInt();        System.out.print("Enter balance: ");
+        final int balance = in.nextInt();
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_user_table","root","0000");
+
+            String sql = "INSERT INTO db_user_name (DB_USER_NAME, DB_USER_password, DB_USER_BALANCE) VALUES (?, ?, ?)";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, name);
+            statement.setInt(2, pass);
+            statement.setInt(3, balance);
+            int rowsInserted = statement.executeUpdate();            if (rowsInserted > 0) {
+                System.out.println("A new user was inserted successfully!");            }
+
+        }catch (Exception e){            e.printStackTrace();
+        }
+    }}
